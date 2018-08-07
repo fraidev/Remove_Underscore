7fb21f16d62d944bb259b35cb6ffc1e8a694cdb2
Felipe.Cardozo <felipe.cardozo@sapiensia.com>
2018-08-07 09:19:50 -0300

    Init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Remove_Underscore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Console.ReadLine();
+            var result = x.Replace("_", " ");
+            Console.WriteLine(result);
+        }
+    }
+}
